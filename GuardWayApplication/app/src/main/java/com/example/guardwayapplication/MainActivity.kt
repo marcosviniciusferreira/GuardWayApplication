cbe685d93d2346acde29ca072f95dce6ca3ec039
Marcos Vinicius Ferreira Paim <viniciusferreirapaim@yahoo.com>
2025-10-30 10:15:33 -0300

    personalização pin no mapa (configurar direito o secret)

--- GuardWayApplication/app/src/main/java/com/example/guardwayapplication/MainActivity.kt
+++ GuardWayApplication/app/src/main/java/com/example/guardwayapplication/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.guardwayapplication
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity() {
+
+    private val places = listOf(
+        Place("Google", LatLng(-23.5868031, -46.684306), "Av. Brg. Faria Lima, 3477 - 18º Andar - Itaim Bibi, São Paulo - SP", 4.8f),
+        Place("Parque Ibirapuera", LatLng(-23.5899619, -46.66747), "Av. República do Líbano, 1111 - Ibirapuera, São Paulo - SP", 4.9f)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,5 +28,34 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+
+        mapFragment.getMapAsync { googleMap ->
+            addMarkers(googleMap)
+        }
+    }
+
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .snippet(place.address)
+                    .position(place.latLng)
+                    .icon(
+                        BitmapHelper.vectorToBitmap(
+                            this,
+                            R.drawable.ic_android_black_24dp,
+                            ContextCompat.getColor(this, R.color.black)
+                        )
+                    )
+            )
+        }
     }
 }
+
+data class Place(
+    val name: String,
+    val latLng: LatLng,
+    val address: String,
+    val rating: Float
+)
